ae2d4e5e6ae853e44a5c31b4bec3ba9c430dceea
tizzifona <tizzifona@gmail.com>
2024-10-14 15:58:57 +0200

    Added button load more and loadMore.js

--- js/characters.js
+++ js/characters.js
@@ -1,15 +1,15 @@
 const requestURL = "https://dragonball-api.com/api/characters"; 
 const charactersContainer = document.getElementById('charactersContainer');
 
+const itemsPerPage = 8;  
 
-export async function fetchCharacters() {
-    const response = await fetch(requestURL);
+export async function fetchCharacters(page) {
+    const response = await fetch(`${requestURL}?page=${page}&limit=${itemsPerPage}`);
     const data = await response.json(); 
     return data.items; 
 }
 
-
-fetchCharacters().then(characters => {
+export function displayCharacters(characters) {
     characters.forEach(character => {
         let image = character.image;
         let name = character.name;
@@ -20,15 +20,15 @@ fetchCharacters().then(characters => {
         let maxKi = character.maxKi;
 
         charactersContainer.innerHTML += `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 18rem; margin: 10px;">
             <img src="${image}" class="card-img-top" alt="${name}">
             <div class="card-body">
                 <h5 class="card-title">${name}</h5>
                 <p class="card-text">${gender} - ${race}</p>
-                <p class="card-text"> Base KI: ${ki}</p>
-                <p class="card-text"> Total KI: ${maxKi}</p>
-                <p class="card-text"> Affiliation: ${affiliation}</p>
+                <p class="card-text">Base KI: ${ki}</p>
+                <p class="card-text">Total KI: ${maxKi}</p>
+                <p class="card-text">Affiliation: ${affiliation}</p>
             </div>
         </div>`;
     });
-});
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import { fetchCharacters } from './characters.js';
+import { loadMoreCharacters } from './loadMore.js';
 
 fetchCharacters().then(() => {
     console.log('Characters have been fetched and displayed.');
